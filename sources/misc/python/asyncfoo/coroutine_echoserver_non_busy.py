c09ad4bdc0106431c9373f783c4f09cc948d7bb9
Abhijit Gadgil <gabhijit@iitbombay.org>
2018-10-31 15:17:02 +0530

    almost there with I/O loop

--- sources/misc/python/asyncfoo/coroutine_echoserver_non_busy.py
+++ sources/misc/python/asyncfoo/coroutine_echoserver_non_busy.py
@@ -1,10 +1,18 @@
 # Implementing the same coroutine based echo server without busy waiting
 # and using epoll to do non-blocking I/O
+# TODO: This is still busy waiting and more clean up is needed
+#
+# In fact I am just going to use things from Dave Beazley's talk.
+# https://www.youtube.com/watch?v=MCs5OvhV9S4
 
 import collections
 import socket
 from types import GeneratorType
 import sys
+import select
+
+# Fugly - get rid of this when we do a proper select
+import time
 
 class ConnectionLost(Exception):
     pass
@@ -13,10 +21,10 @@ def listen_on(sock):
     while True:
         # get the next incoming connection
         # print "listen_on: before yield"
-        connected_socket = nonblocking_accept(sock)
-        val = yield connected_socket
-        if val:
-            print "connected_socket", val.fileno()
+        yield "recv", sock
+        client, addr = sock.accept()
+        # We've a client now, now it's time to go and handle this client
+        yield Task(echo_handler(client))
 
 # Create a scheduler to manage all our coroutines
 # Create a coroutine instance to run the echo_handler on
@@ -28,34 +36,31 @@ def nonblocking_read(sock):
     A coroutine that read's data in a non-blocking manner.
     """
     try:
-        print "here", sock.fileno()
+        # Following read is going to be non-blocking because we
+        # come here from IO loop.
+
         data = sock.recv(1024)
-        if data:
-            print "*****Received Data*****:", "".join(data)
-            yield Task(nonblocking_write(sock, data))
 
         if len(data) == 0:
             print "ConnectionLost"
             raise ConnectionLost("EOF")
 
-        yield data
-
-    except socket.error as e:
-        yield Task(nonblocking_read(sock))
+        return data
     except Exception as e:
         print e
 
 
-def nonblocking_write(*args):
+def nonblocking_write(sock, data):
 
     try:
-        sent = args[0].send(args[1])
+        sent = 0
+        sent = sock.send(data)
         if sent == 0:
             raise ConnectionLost("EOF")
     except socket.error:
         sent = -1
     finally:
-        yield sent
+        return sent
 
 def nonblocking_accept(sock):
 
@@ -75,10 +80,18 @@ def nonblocking_accept(sock):
 def echo_handler(sock):
     while True:
         try:
-            yield Task(nonblocking_read(sock))
+            print "running handler"
+
+            yield "recv", sock
+            data = nonblocking_read(sock)
+
+            if data:
+                yield "send", sock
+                written = nonblocking_write(sock, data)
+
         except ConnectionLost:
             print "ConnectionLost"
-            break
+            return
         except Exception as e:
             print e
             #pass  # exit normally if connection lost
@@ -105,7 +118,6 @@ class Task(object):
         self.sendval = sendval
 
     def run(self):
-        print "target:", self.target.__name__, "isGenerator:", isinstance(self.target, GeneratorType)
         return self.target.send(self.sendval)
 
 
@@ -114,36 +126,73 @@ class Scheduler(object):
     def __init__(self):
         self.taskmap = {}
         self.runqueue = collections.deque()
+        self.epoll = None
+        self.stopped = False
+
+    def wait_for_io(self, why, sock, task):
+        """
+        We need to register interest in I/O events.
+        """
+        if not self.epoll:
+            RuntimeError("Nothing to wait for I/O on.")
+
+        if why in ('recv', 'send'):
+            fd = sock.fileno()
+            if why == 'recv':
+                print("registering for I/O read:", fd)
+                self.epoll.register(fd, select.EPOLLIN)
+            elif why == 'send':
+                print("registering for I/O write:", fd)
+                self.epoll.register(fd, select.EPOLLOUT)
+            self.taskmap[fd] = task
+
+        elif why == 'task':
+            self.schedule(task)
+
+
+    def run(self):
+        self.epoll = select.epoll()
+        self.stopped = False
+
+    def stop(self):
+        self.stopped = True
 
     def schedule(self, task):
+        if not isinstance(task, Task):
+            raise ValueError("{} should be an instance of {}", task, Task)
         self.runqueue.append(task)
 
     def loop(self):
 
-        while len(self.runqueue) > 0:
-            task = self.runqueue.popleft()
-            result = task.run()
-            #print "result:", result
-            if isinstance(result, Task):
-                self.schedule(result)
-            if result is None:
-                self.schedule(task)
+        while True:
+            if self.stopped:
+                break
+            while not self.runqueue:
+                # Wait for some I/O notification
+                print("waiting for IO")
+                events = self.epoll.poll(1)
+                for fileno, event in events:
+                    self.epoll.unregister(fileno)
+                    task = self.taskmap.get(fileno, None)
+                    if task:
+                        self.runqueue.append(task)
+            while self.runqueue:
+                task = self.runqueue.popleft()
+                print("running task:", task.target.__name__)
+                result = task.run()
+                if result:
+                    if isinstance(result, Task):
+                        self.schedule(result)
+                    else:
+                        why, what = result
+                        # So we have something that is waiting for I/O now
+                        # So we need to make sure they are polled on
+                        self.wait_for_io(why, what, task)
 
 s = Scheduler()
 
 s.schedule(Task(server))
 
+s.run()
 s.loop()
-
-"""
-Let's reason about this code
-
-When 'listen_on' is added to the Trampoline at `t.add(server)`
-`t.schedule(listen_on) get's called. This is going to first do a
-listen_on.send(None) in the schedule and bring it to a point where it `yield`s
-for the first time. ie. `yield nonblocking_accept(sock)` so the value
-is now nonblocking_accept - coroutine object and that's added
-
-and `t.run()` is run -
-"""
 
