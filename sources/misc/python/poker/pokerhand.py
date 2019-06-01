d6d08be75f201074baf943e91228297f1aa9cdd2
Abhijit Gadgil <gabhijit@iitbombay.org>
2019-06-01 18:57:13 +0530

    Simulation of poker hands complete

--- sources/misc/python/poker/pokerhand.py
+++ sources/misc/python/poker/pokerhand.py
@@ -12,12 +12,13 @@ def deck(shuffled=False):
         random.shuffle(d)
     return d
 
-class PokerHand(object):
+
+class BasePokerHand:
 
     @classmethod
-    def from_str(cls, hand):
+    def from_str(cls, hand, sep=' '):
 
-        cards = [(x[0], x[1]) for x in hand.split(' ')]
+        cards = [(x[0], x[1]) for x in hand.split(sep)]
         ranks = [cardranks[x[0]] for x in cards]
         suits = [x[1] for x in cards]
 
@@ -33,14 +34,6 @@ class PokerHand(object):
         self._ordered = []
 
     @property
-    def ordered(self):
-        if self._ordered:
-            return self._ordered
-
-        rank_counts = [(i, self._ranks.count(i)) for i in self._ranks]
-        return sorted(rank_counts, key=itemgetter(1, 0), reverse=True)
-
-    @property
     def cards(self):
         return self._cards
 
@@ -51,6 +44,48 @@ class PokerHand(object):
     @property
     def suits(self):
         return self._suits
+
+    @property
+    def ordered(self):
+        if self._ordered:
+            return self._ordered
+
+        rank_counts = list(set([(i, self._ranks.count(i)) for i in self._ranks]))
+        return sorted(rank_counts, key=itemgetter(1, 0), reverse=True)
+
+    @property
+    def score(self):
+
+        score = 1 << 0
+
+        if self.pair(): score <<= 1
+
+        return score
+
+    def __lt__(self, other):
+        if self.score < other.score: return True
+        if self.score > other.score: return False
+
+        for a, b in zip(self.ordered, other.ordered):
+            if a[0] < b[0]:
+                return True
+            if a[0] > b[0]:
+                return False
+
+        return False
+
+    def __repr__(self):
+        return " ".join(["".join([str(x[0]), str(x[1])]) for x in self._cards]) + \
+                " Score: {}".format(self.score)
+
+
+class MiniPokerHand(BasePokerHand):
+
+    def pair(self):
+        return len(set(self._ranks)) == 1
+
+
+class PokerHand(BasePokerHand):
 
     def flush(self):
 
@@ -94,29 +129,33 @@ class PokerHand(object):
 
         return score
 
-    def __lt__(self, other):
-        if self.score < other.score: return True
-        if self.score > other.score: return False
-
-        for a, b in zip(self.ordered, other.ordered):
-            if a[0] < b[0]:
-                return True
-            if a[0] > b[0]:
-                return False
-
-        return False
-
-    def __repr__(self):
-        return " ".join(["".join([str(x[0]), str(x[1])]) for x in self._cards]) + \
-                " Score: {}".format(self.score)
-
 if __name__ == '__main__':
     hand = 'AC 2S 3C 4H 5S'
     assert PokerHand.from_str(hand).straight() == True
+    hand = 'AC 2S 3C 4H 5S'
+    print(PokerHand.from_str(hand).ordered)
     hand = '8C TS JC 9H 7S'
     print("Hand '{}': Is straight? ".format(hand), \
            PokerHand.from_str(hand).straight())
     print("Hand {}': ordered".format(hand), PokerHand.from_str(hand).ordered)
     hand = '8C 8S 8D 8H 7S'
+    print(PokerHand.from_str(hand).ordered)
     print("Hand {}': Four of a kind".format(hand), \
             PokerHand.from_str(hand).four_of_a_kind())
+
+    hand = '8C 8S'
+    mph = MiniPokerHand.from_str(hand)
+    print(mph.score)
+
+    x = '9S:JC 6C:9D 2S:3D 6H:4S QC:7H TD:8D JS:7C 8H:4D 9C:4H:9H:JH:8S 4 0 1 0'
+
+    from itertools import combinations
+
+    h1 = '9S:JC'
+    h2 = '6C:9D'
+    for c in combinations('9C:4H:9H:JH'.split(':'), 3):
+        h11 = ":".join([h1, ":".join(c)])
+        h21 = ":".join([h2, ":".join(c)])
+        ph11 = PokerHand.from_str(h11, sep=":")
+        ph21 = PokerHand.from_str(h21, sep=":")
+        print (h11, ph11.score, h21, ph21.score, ph11 > ph21)
--- sources/misc/python/poker/pokertable.py
+++ sources/misc/python/poker/pokertable.py
@@ -1,4 +1,4 @@
-from pokerhand import PokerHand
+from pokerhand import PokerHand, MiniPokerHand
 from pokerhand import deck
 from itertools import combinations
 
@@ -12,7 +12,7 @@ class PokerPlayer:
         return self._own_cards
 
     def __repr__(self):
-        return ",".join(self._own_cards)
+        return ":".join(self._own_cards)
 
 class PokerTable:
 
@@ -22,6 +22,11 @@ class PokerTable:
         self._community_cards = []
         self._deck = deck
         self._burnt_cards = []
+        self._table_ranks = {}
+        self._deal_winner = -1
+        self._flop_winner = -1
+        self._turn_winner = -1
+        self._river_winner = -1
 
     @property
     def players(self):
@@ -49,39 +54,71 @@ class PokerTable:
         self.burn_card()
         self._community_cards.append(self._deck.pop())
 
+    @property
+    def table_ranks(self):
+        return dict(sorted(self._table_ranks.items(),
+            key=lambda kv: kv[0]))
+
     def __repr__(self):
 
-        return ",".join(self._community_cards)
+        players = " ".join([str(player) for player in self._players])
+        community_cards = ":".join(self._community_cards)
 
-    def rank_players(self):
+        return " ".join([players, community_cards,
+            str(self._deal_winner), str(self._flop_winner),
+            str(self._turn_winner), str(self._river_winner),
+            str(int(self._deal_winner == self._river_winner)),
+            str(int(self._flop_winner == self._river_winner)),
+            str(int(self._turn_winner == self._river_winner))
+            ])
+
+
+    def rank_players(self, pre_flop=False):
+
         player_hands = {}
-        for i, player in enumerate(self._players):
-            possible_hands = []
-            for c in combinations(self._community_cards, 3):
-                hand = player.own_cards + list(c)
-                possible_hands.append(PokerHand.from_str(" ".join(hand)))
-            player_hands[i] = sorted(possible_hands, reverse=True)[0]
+        if pre_flop:
+            for i, player in enumerate(self._players):
+                player_hands[i] = MiniPokerHand.from_str( \
+                        " ".join(player.own_cards))
+        else:
+            for i, player in enumerate(self._players):
+                possible_hands = []
+                for c in combinations(self._community_cards, 3):
+                    hand = player.own_cards + list(c)
+                    possible_hands.append(PokerHand.from_str(" ".join(hand)))
+                player_hands[i] = sorted(possible_hands, reverse=True)[0]
 
         players = (sorted(player_hands.items(),
             key=lambda kv:kv[1], reverse=True))
-        print (dict(players))
+
+        for i in range(len(players)):
+            player_id = players[i][0]
+            self._table_ranks[i] = player_id
+
+    def run_one_pot(self):
+
+        self.deal()
+        self.rank_players(pre_flop=True)
+        self._deal_winner = self._table_ranks[0]
+
+        self.flop()
+        self.rank_players()
+        self._flop_winner = self._table_ranks[0]
+
+        self.turn()
+        self.rank_players()
+        self._turn_winner = self._table_ranks[0]
+
+        self.river()
+        self.rank_players()
+        self._river_winner = self._table_ranks[0]
+
+        self._pot_winner = self._river_winner
+
 
 if __name__ == '__main__':
 
-    table = PokerTable(8, deck(shuffled=True))
-    table.deal()
-
-    for i, player in enumerate(table.players):
-        print (i, ":", player)
-
-    table.flop()
-    print(table)
-    table.rank_players()
-
-    table.turn()
-    print(table)
-    table.rank_players()
-
-    table.river()
-    print(table)
-    table.rank_players()
+    for i in range(5000):
+        table = PokerTable(8, deck(shuffled=True))
+        table.run_one_pot()
+        #print(table)
